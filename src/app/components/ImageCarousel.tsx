6ec6d75dedffb4e12d5bcdc7e918b0d1c78b749f
Robert Safin <robertsafin98@gmail.com>
2023-08-26 16:52:09 +0800

    re

--- src/app/components/ImageCarousel.tsx
+++ src/app/components/ImageCarousel.tsx
@@ -15,7 +15,7 @@ const ImageCarousel:FC<Props> = (props) => {
 
 
   return (
-    <Carousel infiniteLoop={true} autoPlay={true} interval={3000} showThumbs={false} className='shadow-green shadow-md'>
+    <Carousel infiniteLoop={true} interval={3000} showThumbs={false} className='shadow-green shadow-md' >
     {props.photos.map((photo, index) => (
       <div key={index}>
         <Image className={props.imageClasses} src={photo} alt={`photo`} width={10000} height={10000} />
