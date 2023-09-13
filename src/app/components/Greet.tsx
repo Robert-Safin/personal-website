1168ac7afd605a84b401dcfc3b16eb21f6640b39
Robert Safin <robertsafin98@gmail.com>
2023-09-13 14:58:50 +0800

    re

--- src/app/components/Greet.tsx
+++ src/app/components/Greet.tsx
@@ -20,14 +20,13 @@ const Greet = () => {
         .
       </h1>
       <p className="text text-white text-[16px] xl:w-full md:pr-4">
-        I am an aspiring full-stack developer, focusing on react with Next.js. I
-        have 2 degrees from London universities and experience working in real
-        estate development and as an oil and gas economist. I am now looking to
-        transition into a career in software development. I have attended a 3
-        month intensive coding bootcamp `Le Wagon` and currently working and
-        learning on my own projects. I strife to write typesafe, maintainable
-        code as well as provide a great user experience. I am now looking for
-        remote work opportunities.
+        I am a full-stack react developer. I have 2 degrees from London
+        universities and experience working in real estate development and as an
+        oil and gas economist. I have attended a 3 month intensive coding
+        bootcamp `Le Wagon`. I strife to write typesafe, maintainable, scalable
+        code as well as provide a great user experience all on the Edge network
+        to provide fastest response times. I am now looking for remote work
+        opportunities.
       </p>
       <Link href={"#contact"}>
         <p className="mt-20 hover:text-green headerM text-white text-[16px] tracking-widest border-b-2 border-green w-fit mx-auto pb-1 md:mx-0">
